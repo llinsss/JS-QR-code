f61dc335a9ff0e45d3b6ada7b39594be65d6ebac
llins <llinsomoudu@gmail.com>
2025-03-19 18:50:40 +0100

    poly

---
+++ poly.js
@@ -0,0 +1,90 @@
+function GF256Poly(field,  coefficients)
+{
+	if (coefficients == null || coefficients.length == 0)
+	{
+		throw "System.ArgumentException";
+	}
+	this.field = field;
+	var coefficientsLength = coefficients.length;
+	if (coefficientsLength > 1 && coefficients[0] == 0)
+	{
+		// Leading term must be non-zero for anything except the constant polynomial "0"
+		var firstNonZero = 1;
+		while (firstNonZero < coefficientsLength && coefficients[firstNonZero] == 0)
+		{
+			firstNonZero++;
+		}
+		if (firstNonZero == coefficientsLength)
+		{
+			this.coefficients = field.Zero.coefficients;
+		}
+		else
+		{
+			this.coefficients = new Array(coefficientsLength - firstNonZero);
+			for(var i=0;i<this.coefficients.length;i++)this.coefficients[i]=0;
+			//Array.Copy(coefficients, firstNonZero, this.coefficients, 0, this.coefficients.length);
+			for(var ci=0;ci<this.coefficients.length;ci++)this.coefficients[ci]=coefficients[firstNonZero+ci];
+		}
+	}
+	else
+	{
+		this.coefficients = coefficients;
+	}
+	
+	this.__defineGetter__("Zero", function()
+	{
+		return this.coefficients[0] == 0;
+	});
+	this.__defineGetter__("Degree", function()
+	{
+		return this.coefficients.length - 1;
+	});
+	this.__defineGetter__("Coefficients", function()
+	{
+		return this.coefficients;
+	});
+	
+	this.getCoefficient=function( degree)
+	{
+		return this.coefficients[this.coefficients.length - 1 - degree];
+	}
+	
+	this.evaluateAt=function( a)
+	{
+		if (a == 0)
+		{
+			// Just return the x^0 coefficient
+			return this.getCoefficient(0);
+		}
+		var size = this.coefficients.length;
+		if (a == 1)
+		{
+			// Just the sum of the coefficients
+			var result = 0;
+			for (var i = 0; i < size; i++)
+			{
+				result = GF256.addOrSubtract(result, this.coefficients[i]);
+			}
+			return result;
+		}
+		var result2 = this.coefficients[0];
+		for (var i = 1; i < size; i++)
+		{
+			result2 = GF256.addOrSubtract(this.field.multiply(a, result2), this.coefficients[i]);
+		}
+		return result2;
+	}
+	
+	this.addOrSubtract=function( other)
+		{
+			if (this.field != other.field)
+			{
+				throw "GF256Polys do not have same GF256 field";
+			}
+			if (this.Zero)
+			{
+				return other;
+			}
+			if (other.Zero)
+			{
+				return this; }
